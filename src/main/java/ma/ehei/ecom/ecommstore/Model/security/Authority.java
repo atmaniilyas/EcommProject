f02cddd99064bf13e4b42f1506a40026c6182008
Ilyas <atmanuilyas@gmail.com>
2022-12-27 22:17:58 +0100

    Config and model added

---
+++ src/main/java/ma/ehei/ecom/ecommstore/Model/security/Authority.java
@@ -0,0 +1,14 @@
+package ma.ehei.ecom.ecommstore.Model.security;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public class Authority implements GrantedAuthority {
+    private final String authority;
+    public Authority(String authority){
+        this.authority = authority;
+    }
+    @Override
+    public String getAuthority(){
+        return authority;
+    }
+}
---
+++ src/main/java/ma/ehei/ecom/ecommstore/Model/security/UserRole.java
@@ -0,0 +1,47 @@
+package ma.ehei.ecom.ecommstore.Model.security;
+
+import jakarta.persistence.*;
+import ma.ehei.ecom.ecommstore.Model.User;
+
+@Entity
+@Table(name = "user_role")
+public class UserRole {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userRoleId;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User user;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    public UserRole(User user, Role role){
+        this.user = user;
+        this.role = role;
+    }
+
+    public Long getUserRoleId() {
+        return userRoleId;
+    }
+
+    public void setUserRoleId(Long userRoleId) {
+        this.userRoleId = userRoleId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
